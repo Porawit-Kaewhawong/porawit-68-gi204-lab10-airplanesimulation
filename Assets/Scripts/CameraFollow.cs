a5ca9fd5f403b1c7596d7f1dcda01eb498f114ac
Porawit-Kaewhawong <porawit.kaew@bumail.net>
2026-03-17 15:54:35 +0700

    CameraFollow.cs Updated

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,13 +3,8 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public Vector3 offset;
+    public Vector3 offset = new Vector3(0, 5, -10);
     public float smoothSpeed = 5f;
-
-    void Start()
-    {
-        offset = transform.position;    
-    }
 
     void LateUpdate()
     {
